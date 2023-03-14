c8f92df405bcd5252c17864d183a1f0994f1a847
tashi1990 <tashipetter@hotmail.co.uk>
2023-03-14 14:25:56 +0100

    Added weather icon.
    Improved layout with some extra CSS/HTML

--- src/app.js
+++ src/app.js
@@ -30,6 +30,7 @@ function displayData(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let mainIconElement = document.querySelector("#main-icon");
 
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
@@ -37,10 +38,14 @@ function displayData(response) {
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = response.data.wind.speed;
   dateElement.innerHTML = formatDate(response.data.time * 1000);
+  mainIconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
 }
 
 let apiKey = `24boa6cedt6f70a95aa73af3f70e5464`;
-let city = "Berlin";
+let city = "London";
 let units = "metric";
 let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
 

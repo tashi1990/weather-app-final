43af3e4a560d658fe15be6f36739d3344ea9355c
tashi1990 <tashipetter@hotmail.co.uk>
2023-03-14 11:24:52 +0000

    Showing current temperature, wind, humidity and weather description

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
+function displayData(response) {
+  console.log(response.data.condition.description);
+  let temperatureElement = document.querySelector("#current-temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = response.data.wind.speed;
+}
 
+let apiKey = `24boa6cedt6f70a95aa73af3f70e5464`;
+let city = "Berlin";
+let units = "metric";
+let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
+
+axios.get(apiURL).then(displayData);
